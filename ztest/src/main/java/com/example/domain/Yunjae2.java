8cd2f4e22a029d0401832286dfd94796c14cacc6
yoonmi0323 <kosmo@DESKTOP-12HMH9R>
2023-06-28 16:40:11 +0900

    sdfsd

--- ztest/src/main/java/com/example/domain/Yunjae2.java
+++ ztest/src/main/java/com/example/domain/Yunjae2.java
@@ -2,11 +2,11 @@ package com.example.domain;
 
 public class Yunjae2 {
 	
-	// 황윤재 이 꼽같은 녀석
-	// 니가 몬대 나한테 알밥을 머그래
-	// 오므라이스도 알밥도 맛있다
+	// 황윤재 이 꼽같은 녀석2
+	// 니가 몬대 나한테 알밥을 머그래2
+	// 오므라이스도 알밥도 맛있다2
 
-	// 윤미는 바보라는 뜻
+	// 윤미는 바보라는 뜻2
 	private String yoonmibabo;
 	
 	
